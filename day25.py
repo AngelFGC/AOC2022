19fc21e8eeace17542f35114f68bc72c1008ffe6
Angel FGC <44098660+AngelFGC@users.noreply.github.com>
2023-01-06 10:42:19 +0900

    Day 25 complete

--- day25.py
+++ day25.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import List
+from typing import Any, List
 
 SNAFUDIGITS = {
     "=":-2,
@@ -13,25 +13,63 @@ DIGITSNAFU = {SNAFUDIGITS[k]:k for k in SNAFUDIGITS}
 
 class SNAFU(object):
     digits:List[int]
+
+    def __init__(self, s:Any) -> None:
+        self.digits = list()
+        if isinstance(s, str):
+            for c in s[::-1]:
+                self.digits.append(SNAFUDIGITS[c])
+        elif isinstance(s, list):
+            for c in s:
+                if not isinstance(c, int) or not (-2 <= c <= 2):
+                    raise ValueError()
+                self.digits.append(c)
+        else:
+            raise ValueError()
     
-    def __init__(self, s:str) -> None:
-        for c in s[::-1]:
-            self.digits.append(SNAFUDIGITS[c])
-    
+    def __radd__(self, other):
+        if other == 0:
+            return self
+        else:
+            return self.__add__(other)
+
+    def __len__(self):
+        return len(self.digits)
+
     def __add__(self, other:SNAFU) -> SNAFU:
-        pass
+        left = self.digits[:] + ([0] * (len(other) - len(self))) + [0]
+        right = other.digits[:] + ([0] * (len(self) - len(other))) + [0]
+        rplusl = [0]*len(right)
+        for i,(x,y) in enumerate(zip(left, right)):
+            rplusl[i] = x + y
+
+        swapped = True
+        while swapped:
+            swapped = False
+            for i, c in enumerate(rplusl):
+                if c < -2:
+                    swapped = True
+                    rplusl[i] = 5 + c
+                    rplusl[i+1] -= 1
+                elif c > 2:
+                    swapped = True
+                    rplusl[i] = c - 5
+                    rplusl[i+1] += 1
+
+        for i in range(len(rplusl)-1, -1, -1):
+            if rplusl[i] == 0:
+                del rplusl[i]
+            else:
+                break
+
+        return SNAFU(rplusl)
+
 
     def toDecimal(self):
-        s = 0
-        for i, d in enumerate(self.digits):
-            s += d*(5**i)
-        return s
+        return sum(d*(5**i) for i, d in enumerate(self.digits))
     
     def toSNAFUstr(self):
-        s = ""
-        for d in self.digits[::-1]:
-            s += DIGITSNAFU[d]
-        return s
+        return "".join(DIGITSNAFU[d] for d in self.digits[::-1])
     
     def __repr__(self) -> str:
         return repr(self.digits)
@@ -39,13 +77,25 @@ class SNAFU(object):
     def __str__(self) -> str:
         return f"{self.toSNAFUstr()} >> {str(self.toDecimal())}"
 
-def day24_read():
-    with open("inputs/day24.txt", mode="r+", encoding="utf-8") as f:
-        return f.readlines()
+def day25_read() -> List[SNAFU]:
+    with open("inputs/day25.txt", mode="r+", encoding="utf-8") as f:
+        return [SNAFU(line.strip()) for line in f]
     
 
 def day25():
-    pass
+    # x = SNAFU("1=-0-2")
+    # y = SNAFU("12111")
+    # z = x + y
+    # print(str(z))
+
+    # x, y  = 1747, 906
+    # z = x + y
+    # z = str(z)
+    # print(z)
+    numbers = day25_read()
+    s = sum(numbers)
+    print(s)
+
 
 if __name__ == "__main__":
     day25()
